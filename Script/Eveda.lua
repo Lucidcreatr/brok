bf80904c5a294eb1514a184d469ed2ac8c408ebd
Lucidcreatr <lpprojecttt06@gmail.com>
2025-06-11 22:29:35 +0300

    Update Eveda.lua

--- Script/Eveda.lua
+++ Script/Eveda.lua
@@ -5,7 +5,6 @@ local Window = OrionLib:MakeWindow({
 	HidePremium = false,
 	SaveConfig = true,
 	ConfigFolder = "EvadeDraconisConfig"
-
 })
 
 local MainTab = Window:MakeTab({
@@ -14,6 +13,15 @@ local MainTab = Window:MakeTab({
 	PremiumOnly = false
 })
 
+-- Güvenli Humanoid erişimi
+local function getHumanoid()
+	local plr = game.Players.LocalPlayer
+	local char = plr.Character or plr.CharacterAdded:Wait()
+	return char:WaitForChild("Humanoid"), char
+end
+
+-- Speed Hack
+local targetSpeed = 16
 MainTab:AddSlider({
 	Name = "Speed Hack",
 	Min = 16,
@@ -23,10 +31,12 @@ MainTab:AddSlider({
 	Increment = 1,
 	ValueName = "Speed",
 	Callback = function(Value)
-		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+		targetSpeed = Value
 	end
 })
 
+-- Jump Hack
+local targetJump = 50
 MainTab:AddSlider({
 	Name = "Jump Hack",
 	Min = 50,
@@ -36,29 +46,63 @@ MainTab:AddSlider({
 	Increment = 1,
 	ValueName = "JumpPower",
 	Callback = function(Value)
-		game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+		targetJump = Value
 	end
 })
+
+-- Değerleri anti-cheat’e karşı sürekli sabitle
+game:GetService("RunService").RenderStepped:Connect(function()
+	local humanoid = getHumanoid()
+	if humanoid then
+		if humanoid.WalkSpeed ~= targetSpeed then
+			humanoid.WalkSpeed = targetSpeed
+		end
+		if humanoid.JumpPower ~= targetJump then
+			humanoid.JumpPower = targetJump
+		end
+	end
+end)
+
+-- Fly sistemi
+local flyEnabled = false
+local function addFly(char)
+	local root = char:WaitForChild("HumanoidRootPart")
+	if not root:FindFirstChild("EvadeFLY") then
+		local bv = Instance.new("BodyVelocity")
+		bv.Name = "EvadeFLY"
+		bv.MaxForce = Vector3.new(1e5, 1e5, 1e5)
+		bv.Velocity = Vector3.new(0, 50, 0)
+		bv.Parent = root
+	end
+end
 
 MainTab:AddToggle({
 	Name = "Fly hack (Beta)",
 	Default = false,
 	Callback = function(state)
 		local plr = game.Players.LocalPlayer
-		local char = plr.Character
-		local humanoid = char:FindFirstChildOfClass("Humanoid")
-		if state then
-			local bodyVelocity = Instance.new("BodyVelocity", char.HumanoidRootPart)
-			bodyVelocity.Velocity = Vector3.new(0, 50, 0)
-			bodyVelocity.MaxForce = Vector3.new(100000, 100000, 100000)
-			bodyVelocity.Name ="EvadeFLY"
+		flyEnabled = state
+
+		if flyEnabled then
+			addFly(plr.Character or plr.CharacterAdded:Wait())
+			plr.CharacterAdded:Connect(function(char)
+				if flyEnabled then
+					wait(1)
+					addFly(char)
+				end
+			end)
 		else
-			local fly = char.HumanoidRootPart:FindFirstChild("EvadeFLY")
-			if fly then fly:Destroy() end
+			local char = plr.Character
+			if char then
+				local fly = char:FindFirstChild("HumanoidRootPart"):FindFirstChild("EvadeFLY")
+				if fly then
+					fly:Destroy()
+				end
+			end
 		end
 	end
 })
 
-MainTab:AddParagraph("Uyarı","Hile şuanda ACC nedeniyle kötü ve ya hatalı çalışabilir LCX ekimiz en yakın zamanda çözücekdir")
+MainTab:AddParagraph("Uyarı", "Hile şuanda ACC nedeniyle kötü veya hatalı çalışabilir. LCX ekibimiz en yakın zamanda çözecektir.")
 
 OrionLib:Init()
